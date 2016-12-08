64b21c5f19001a7b754750cbad0a2241afc54761
sancansion <sancansion@gmail.com>
2016-12-08 20:40:42 +0900

    add pages

---
+++ system.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+<meta name="author" content="FREEHTML5.CO" />
+
+<!-- Facebook and Twitter integration -->
+<meta property="og:title" content="ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋" />
+<meta property="og:image" content="website" />
+<meta property="og:url" content="http://missmrs.biz/" />
+<meta property="og:site_name" content="ミスミセス" />
+<meta property="og:description" content="当店Miss.Mrs(ミスミセス)は大阪市北区は南森町.扇町.天六.ミナミ(中央区)には長堀橋にある完全予約制のプライベートメンズエステです。" />
+<!--    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" /> -->
+
+<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+
+<!-- Animate.css -->
+<link rel="stylesheet" href="css/animate.css">
+<!-- Icomoon Icon Fonts-->
+<link rel="stylesheet" href="css/icomoon.css">
+<!-- Bootstrap  -->
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<!-- Magnific Popup -->
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<!-- Owl Carousel  -->
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+<!-- Theme style  -->
+<link rel="stylesheet" href="css/style.css">
+
+<!-- Modernizr JS -->
+<script src="js/modernizr-2.6.2.min.js"></script>
+<!-- FOR IE9 below -->
+<!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	<div class="fh5co-loader"></div>
+
+	<div id="page">
+		<nav class="fh5co-nav" role="navigation">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+						<div id="fh5co-logo">
+							<a href="index.html">Miss.Mrs</a>
+						</div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active">
+								<a href="index.php">TOP</a>
+							</li>
+							<li>
+								<a href="therapist.php">Therapist</a>
+							</li>
+							<li>
+								<a href="schedule.php">Schedule</a>
+							</li>
+							<li>
+								<a href="system.php">System</a>
+							</li>
+							<li>
+								<a href="recruit.php">Recruit</a>
+							</li>
+							<li class="has-dropdown">
+								<a href="access.php">Access</a>
+								<ul class="dropdown">
+									<li>
+										<a href="#tenroku">天神橋六丁目</a>
+									</li>
+									<li>
+										<a href="#ougi">扇町</a>
+									</li>
+									<li>
+										<a href="#nanmori">南森町</a>
+									</li>
+									<li>
+										<a href="#nagahori">長堀橋</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<!-- START:header -->
+		<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image: url(images/img_bg_2.jpg);"
+			data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center">
+						<div class="display-t">
+							<div class="display-tc animate-box" data-animate-effect="fadeIn">
+								<h1>Miss.Mrs</h1>
+								<h2>ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- END:header -->
+		<!-- START:Main -->
+		<!-- START: -->
+		<div id="fh5co-couple">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6  text-left fh5co-heading animate-box">right</div>
+					<div class="col-md-6  text-left fh5co-heading animate-box">left</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div id="fh5co-services" class="fh5co-section-gray">
+			<div class="container">
+
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+						<h2>料金表</h2>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+							<span class="icon">
+								<i class="icon-heart"></i>
+							</span>
+							<div class="icon-heart">
+								<h3>60分・・・9,000円</h3>
+
+							</div>
+						</div>
+
+						<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+							<span class="icon">
+								<i class="icon-heart"></i>
+							</span>
+							<div class="icon-heart">
+								<h3>90分・・・11,000円</h3>
+							</div>
+						</div>
+
+						<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+							<span class="icon">
+								<i class="icon-heart"></i>
+							</span>
+							<div class="icon-heart">
+								<h3>120分・・・15,000円</h3>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+							<span class="icon">
+								<i class="icon-heart"></i>
+							</span>
+							<div class="icon-heart">
+								<h3>延長30分・・・5,000円</h3>
+							</div>
+						</div>
+
+						<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+							<span class="icon">
+								<i class="icon-heart"></i>
+							</span>
+							<div class="icon-heart">
+								<h3>セラピスト指名料 ・・・50円</h3>
+							</div>
+						</div>
+
+						<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+							<span class="icon">
+								<i class="icon-heart"></i>
+							</span>
+							<div class="icon-heart">
+
+								<p>※60分コースは時間の都合上、背面のみの施術となりますので予めご了承ください。</p>
+							</div>
+						</div>
+
+						<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+							<span class="icon">
+								<i class="icon-heart"></i>
+							</span>
+							<div class="icon-heart">
+								<p>※120分以上のコースはセラピストオリジナルメニューを実施します。</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-couple">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-12  text-left fh5co-heading animate-box">
+						<h2>注意事項</h2>
+						<p>
+							お電話にての御予約を優先させて頂きます。
+							<br>
+							<br>
+							メールでの御予約の際はお 名前・電話番号を一緒にお送りくださるとスムーズに御案内ができますので 御協力よろしくお願いします。
+							<br>
+							<br>
+
+							ご質問等あれば.電話.メール.ラインなどでお気軽にお問い合わせ下さい。
+							<br>
+							<br>
+							当店は風俗店ではありません。アロマセラピー、マッサージ、リラクゼーション、メンズエステ等のボディケアを目的としており風俗店では御座いません。
+							<br>
+							<br>
+							下記に適当される方のご利用はお断りさせて頂きます。
+							<br>
+							<br>
+							またはやむを得ず施術を中止させて頂く場合が御座います。その際は料金の返金は致しませのであらかじめご了承ください。
+							<br>
+							<br>
+							・暴力団関係者またはそれに準ずる 方、刺青等ある方。
+							<br>
+							・泥酔されてる方。
+							<br>
+							・重度の水虫、皮膚炎のある方。
+							<br>
+							・同業者及びスカウト行為をされた 方。
+							<br>
+							・無断キャンセルをされた方
+							<br>
+							・性的サービスを要求される方
+							<br>
+							・セラピストに対するタッチ・暴言 暴力その他のセラピストが嫌がる 行為をされる方。
+							<br>
+							・当店にふさわしく無いと判断され る方。
+							<br>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END: -->
+	<!-- END:Main -->
+	<!-- START:footer -->
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2017 MissMrs All Rights Reserved.</small>
+					</p>
+					<ul class="fh5co-social-icons">
+						<li>
+							<a href="https://www.instagram.com/missmrs01/">
+								<i class="icon-instagram"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.facebook.com/MissMrs-747337258740161/?fref=hovercard">
+								<i class="icon-facebook"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- END:footer -->
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop">
+			<i class="icon-arrow-up"></i>
+		</a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
+	<script src="js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+</body>
+</html>
