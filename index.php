f59fdb30f627f26034be1dbd871034ff4dc20137
bytemouse <bytemouse@localhost.localdomain>
2016-12-08 17:10:55 +0900

    move files

---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+<meta name="author" content="FREEHTML5.CO" />
+
+<!-- Facebook and Twitter integration -->
+<meta property="og:title" content="ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋" />
+<meta property="og:image" content="website" />
+<meta property="og:url" content="http://missmrs.biz/" />
+<meta property="og:site_name" content="ミスミセス" />
+<meta property="og:description" content="当店Miss.Mrs(ミスミセス)は大阪市北区は南森町.扇町.天六.ミナミ(中央区)には長堀橋にある完全予約制のプライベートメンズエステです。" />
+<!--    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" /> -->
+
+<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+
+<!-- Animate.css -->
+<link rel="stylesheet" href="css/animate.css">
+<!-- Icomoon Icon Fonts-->
+<link rel="stylesheet" href="css/icomoon.css">
+<!-- Bootstrap  -->
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<!-- Magnific Popup -->
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<!-- Owl Carousel  -->
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+<!-- Theme style  -->
+<link rel="stylesheet" href="css/style.css">
+
+<!-- Modernizr JS -->
+<script src="js/modernizr-2.6.2.min.js"></script>
+<!-- FOR IE9 below -->
+<!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	<div id="fb-root"></div>
+	<script>
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id))
+				return;
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.8";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
+
+	<div class="fh5co-loader"></div>
+
+	<div id="page">
+		<nav class="fh5co-nav" role="navigation">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+						<div id="fh5co-logo">
+							<a href="index.html">Miss.Mrs</a>
+						</div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active">
+								<a href="index.php">TOP</a>
+							</li>
+							<li>
+								<a href="therapist.php">Therapist</a>
+							</li>
+							<li>
+								<a href="schedule.php">Schedule</a>
+							</li>
+							<li>
+								<a href="system.php">System</a>
+							</li>
+							<li>
+								<a href="recruit.php">Recruit</a>
+							</li>
+							<li class="has-dropdown">
+								<a href="access.php">Access</a>
+								<ul class="dropdown">
+									<li>
+										<a href="#">天神橋六丁目</a>
+									</li>
+									<li>
+										<a href="#">扇町</a>
+									</li>
+									<li>
+										<a href="#">南森町</a>
+									</li>
+									<li>
+										<a href="#">長堀橋</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</nav>
+		<!-- START:header -->
+		<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image: url(images/img_bg_2.jpg);"
+			data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center">
+						<div class="display-t">
+							<div class="display-tc animate-box" data-animate-effect="fadeIn">
+								<h1>Miss.Mrs</h1>
+								<h2>ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- END:header -->
+		<!-- START:Main -->
+		<div id="fh5co-couple">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-left fh5co-heading animate-box">
+						<h2>Miss.Mrs</h2>
+						<h3>当店Miss.Mrs(ミスミセス)は大阪市北区は南森町.扇町.天六.ミナミ(中央区)には長堀橋にある完全予約制のプライベートメンズエステです。</h3>
+						<p>営業時間は午前11:00〜翌朝5:00迄(最終受付午前3:00)全室駅から近く各駅から2分以内の綺麗なシャワー完備の完全個室型になっております。</p>
+						<p>20代〜40代の愛嬌、品、マナーのある素敵な女性が多数在籍しており各セラピストによる貴方様に合った癒しを提供しています。</p>
+						<p>セラピストの技術向上の為、当店では研修も徹底している為、お好みの箇所などお気軽にお申し付け下さい。</p>
+						<p>当店は、リピーター様を大事にがコンセプトで受付時には、是非セラピストのタイプをお申し付け下さい。</p>
+						<p>新規のお客様に置かれましても、リピーター様になって頂ける様に、お客様に合ったセラピストを御案内致します！</p>
+						<p>あそこに行けば間違い無いと言われる様、これからも多数の新人セラピストの導入各セラピストの技術向上、接客スタイルなど誠心誠意向上していく所存です。</p>
+						<p>皆様に最高の癒しと安らぎを提供出来ます様、日々努力致しますので、メンズエステMiss.Mrs(ミスミセス)をこれからもよろしくお願い致します！</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- START:blog -->
+		<div id="fh5co-couple">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6  text-left fh5co-heading animate-box">
+						<h2>Mrs.Miss Blog</h2>
+						<?php
+                                // モジュールを読み込む
+                                require_once("./magpierss/rss_fetch.inc");
+
+                                // キャッシュディレクトリへのパスを設定
+                                // キャッシュディレクトリをchmod 777 (or 707 or 700)
+                                define("MAGPIE_CACHE_DIR", "/path/2/cache/");
+                                // 日本語バケないためにOUTPUT_ENCODINGをUTF-8にする
+                                define("MAGPIE_OUTPUT_ENCODING","UTF-8");
+                                // 取得したいRSSのurl
+                                $url = "http://blog.livedoor.jp/missmrs02/atom.xml";
+                                // ブログのタイトルは$rss->channel['title']という変数、
+                                // RSSは$rss->itemという変数に入る。
+                                // key値、link/title/descriptionで取得できる。
+                                $rss = fetch_rss( $url );
+                                //var_dump ($rss);
+                                $max_lines = 10;
+                                $line = 0;
+                                //echo "". mb_convert_encoding($rss->channel['title'],"UTF-8","auto");
+                                foreach ($rss->items as $item) {
+                                $link = mb_convert_encoding($item['link'],"UTF-8","auto");
+                                $title = mb_convert_encoding($item['title'],"UTF-8","auto");
+                                $date = date("Y/m/d H:i", strtotime($item['modified']));
+                                if (preg_match("/PR$/", $title) != 0) continue;
+                                if (preg_match("/^PR:.+$/", $title) != 0) continue;
+                                if ($line++ == $max_lines) break;
+                                //echo "<a href=$link>$title</a><br />$description";
+                                echo "<dl><dt>$date</dt><dd><a href=\"$link\" target=\"_blank\">$title</a></dd></dl>";
+                                }
+                                 ?>
+
+
+						<a href="http://blog.livedoor.jp/missmrs02/" target="_blank" class="more">
+							Mrs.Miss Blogはこちら <span>▲</span>
+						</a>
+					</div>
+
+					<div class="col-md-6  text-left fh5co-heading animate-box">
+						<h2>出勤情報</h2>
+						<?php
+                        // モジュールを読み込む
+                        require_once("./magpierss/rss_fetch.inc");
+
+                        $url = "http://missmrs01.blog.fc2.com/?xml";
+                        // ブログのタイトルは$rss->channel['title']という変数、
+                        // RSSは$rss->itemという変数に入る。
+                        // key値、link/title/descriptionで取得できる。
+                        $rss = fetch_rss( $url );
+                        //var_dump ($rss)more;
+                        $max_lines = 10;
+                        $line = 0;
+                        //echo "". mb_convert_encoding($rss->channel['title'],"UTF-8","auto");
+                        foreach ($rss->items as $item) {
+                        $link = mb_convert_encoding($item['link'],"UTF-8","auto");
+                        $title = mb_convert_encoding($item['title'],"UTF-8","auto");
+                        $description = mb_convert_encoding($item['description'],"UTF-8","auto");
+                        //$date = date("Y/m/d H:i", intval($item['date_timestamp']));
+                        if (preg_match("/PR$/", $title) != 0) continue;
+                        if (preg_match("/^PR:.+$/", $title) != 0) continue;
+                        if ($line++ == $max_lines) break;
+                        //echo "<a href=$link>$title</a><br />$description";
+                        echo "<dl><dt>$date</dt><dd><a href=\"$link\" target=\"_blank\">$title</a></dd></dl>";
+                        }
+                        ?>
+
+						<a href="http://missmrs01.blog.fc2.com/" target="_blank" class="more">
+							出勤情報Blogはこちら <span>▲</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END:blog -->
+
+		<!-- START:SNS -->
+		<div id="fh5co-couple">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6  text-center fh5co-heading animate-box">
+						<h2>facedbook</h2>
+						<!-- 	<div class="fb-page" data-href="https://www.facebook.com/MissMrs-747337258740161/" data-tabs="timeline" data-small-header="false"
+							data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+							<blockquote cite="https://www.facebook.com/MissMrs-747337258740161/" class="fb-xfbml-parse-ignore">
+								<a href="https://www.facebook.com/MissMrs-747337258740161/">Miss.Mrs</a>
+							</blockquote>
+						</div> -->
+					</div>
+
+					<div class="col-md-6  text-center fh5co-heading animate-box">
+						<h2>instagram</h2>
+						<!-- <div style="position: relative; height: 16px; width: 100%; overflow: hidden; padding-top: 100%;">
+							<iframe src="//widgets-code.websta.me/w/7001f4603d51?ck=MjAxNi0xMi0wNFQwNzowMTozOS4wMDBa" class="websta-widgets"
+								allowtransparency="true" frameborder="0" scrolling="no" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
+							WEBSTA WIDGETS - widgets.websta.me
+						</div> -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END:SNS -->
+
+
+
+		<!-- END:Main -->
+		<!-- START:footer -->
+		<footer id="fh5co-footer" role="contentinfo">
+			<div class="container">
+				<div class="row copyright">
+					<div class="col-md-12 text-center">
+						<p>
+							<small class="block">&copy; 2017 MissMrs All Rights Reserved.</small>
+						</p>
+						<ul class="fh5co-social-icons">
+							<li>
+								<a href="https://www.instagram.com/missmrs01/">
+									<i class="icon-instagram"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com/MissMrs-747337258740161/?fref=hovercard">
+									<i class="icon-facebook"></i>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- END:footer -->
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop">
+			<i class="icon-arrow-up"></i>
+		</a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
+	<script src="js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+</body>
+</html>
