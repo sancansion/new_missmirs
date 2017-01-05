721fa49621671fbd0462ca91869861fcbc1c4903
sancansion <sancansion@gmail.com>
2017-01-05 16:43:53 +0900

    change therapist.php include

--- therapist.php
+++ therapist.php
@@ -8,103 +8,30 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋</title>
+<title>セラピスト一覧 | ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-<meta name="author" content="FREEHTML5.CO" />
+<meta name="author" content="Miss Mrs" />
 
-<!-- Facebook and Twitter integration -->
-<meta property="og:title" content="ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋" />
-<meta property="og:image" content="website" />
-<meta property="og:url" content="http://missmrs.biz/" />
-<meta property="og:site_name" content="ミスミセス" />
-<meta property="og:description" content="当店Miss.Mrs(ミスミセス)は大阪市北区は南森町.扇町.天六.ミナミ(中央区)には長堀橋にある完全予約制のプライベートメンズエステです。" />
-<!--    <meta name="twitter:title" content="" />
-    <meta name="twitter:image" content="" />
-    <meta name="twitter:url" content="" />
-    <meta name="twitter:card" content="" /> -->
+<meta name="description" content="ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋" />
+<meta name="keywords" content="ミスミセス | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 | 南森町 | 長堀橋" />
 
-<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
-<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+<!-- START:SNS meta-->
+<?php include("common/sns_meta.html"); ?>
+<!-- END:SNS meta-->
 
-<!-- Animate.css -->
-<link rel="stylesheet" href="css/animate.css">
-<!-- Icomoon Icon Fonts-->
-<link rel="stylesheet" href="css/icomoon.css">
-<!-- Bootstrap  -->
-<link rel="stylesheet" href="css/bootstrap.css">
-
-<!-- Magnific Popup -->
-<link rel="stylesheet" href="css/magnific-popup.css">
-
-<!-- Owl Carousel  -->
-<link rel="stylesheet" href="css/owl.carousel.min.css">
-<link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-<!-- Theme style  -->
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/style_line.css">
-
-<!-- Modernizr JS -->
-<script src="js/modernizr-2.6.2.min.js"></script>
-<!-- FOR IE9 below -->
-<!--[if lt IE 9]>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-
+<!-- START:SNS meta-->
+<?php include("common/css.html"); ?>
+<!-- END:SNS meta-->
 </head>
 <body>
 	<div class="fh5co-loader"></div>
 
 	<div id="page">
-		<nav class="fh5co-nav" role="navigation">
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-2">
-						<div id="fh5co-logo">
-							<a href="index.php">Miss.Mrs</a>
-						</div>
-					</div>
-					<div class="col-xs-10 text-right menu-1">
-						<ul>
-							<li class="active">
-								<a href="index.php">TOP</a>
-							</li>
-							<li>
-								<a href="therapist.php">Therapist</a>
-							</li>
-							<li>
-								<a href="http://blog.livedoor.jp/missmrs05/" target="_blank">Schedule</a>
-							</li>
-							<li>
-								<a href="system.php">System</a>
-							</li>
-							<li>
-								<a href="recruit.php">Recruit</a>
-							</li>
-							<li class="has-dropdown">
-								<a href="access.php">Access</a>
-								<ul class="dropdown">
-									<li>
-										<a href="#tenroku">天神橋六丁目</a>
-									</li>
-									<li>
-										<a href="#ougi">扇町</a>
-									</li>
-									<li>
-										<a href="#nanmori">南森町</a>
-									</li>
-									<li>
-										<a href="#nagahori">長堀橋</a>
-									</li>
-								</ul>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</nav>
+
+		<!-- START:Navigation-->
+		<?php include("common/navi.html"); ?>
+		<!-- END:Navigation-->
+
 		<!-- START:header -->
 		<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image: url(images/img_bg_2.jpg);"
 			data-stellar-background-ratio="0.5">
@@ -409,85 +336,26 @@
 				</div>
 			</div>
 			<!-- End: Mrs-->
-			<!-- END:Main -->
-			<!-- START:footer -->
-			<footer id="fh5co-footer" role="contentinfo">
-				<div class="container">
-					<div class="row copyright">
-						<div class="col-md-12 text-center">
-							<p>
-								<small class="block">&copy; 2017 MissMrs All Rights Reserved.</small>
-							</p>
-						</div>
-					</div>
-				</div>
-			</footer>
-			<!-- END:footer -->
-
-			<!-- START:footer menu -->
-			<ul class="footer_menu">
-
-				<li>
-					<a href="tel:090-3053-0184">
-						<i class="icon-phone"></i>
-						<br>
-						TEL
-					</a>
-				</li>
-				<li>
-					<a href="mailto:missmrs@docomo.ne.jp">
-						<i class="icon-mail"></i>
-						<br>
-						MAIL
-					</a>
-				</li>
-				<li>
-					<a href="http://line.me/ti/p/LIL-_8S4Qh">
-						<i class="icon-line"></i>
-
-						<br>
-						LINE
-					</a>
-				</li>
-				<li>
-					<a href="recruit.php" class="">
-						<i class="icon-heart"></i>
-						<br>
-						求人
-					</a>
-				</li>
-			</ul>
 		</div>
+		<!-- END:Main -->
+
+		<!-- START:footer -->
+		<?php include("common/footer.html"); ?>
+		<!-- END:footer -->
+
+		<!-- START:footer menu -->
+		<?php include("common/footer_menu.html"); ?>
 		<!-- END:footer menu-->
 
-		<div class="gototop js-top">
-			<a href="#" class="js-gotop">
-				<i class="icon-arrow-up"></i>
-			</a>
-		</div>
+	</div>
 
-		<!-- jQuery -->
-		<script src="js/jquery.min.js"></script>
-		<!-- jQuery Easing -->
-		<script src="js/jquery.easing.1.3.js"></script>
-		<!-- Bootstrap -->
-		<script src="js/bootstrap.min.js"></script>
-		<!-- Waypoints -->
-		<script src="js/jquery.waypoints.min.js"></script>
-		<!-- Carousel -->
-		<script src="js/owl.carousel.min.js"></script>
-		<!-- countTo -->
-		<script src="js/jquery.countTo.js"></script>
 
-		<!-- Stellar -->
-		<script src="js/jquery.stellar.min.js"></script>
-		<!-- Magnific Popup -->
-		<script src="js/jquery.magnific-popup.min.js"></script>
-		<script src="js/magnific-popup-options.js"></script>
+	<!-- START: goto top -->
+	<?php include("common/gototop.html"); ?>
+	<!-- END: goto top -->
 
-		<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
-		<script src="js/simplyCountdown.js"></script>
-		<!-- Main -->
-		<script src="js/main.js"></script>
+	<!-- START: Inclide JS -->
+	<?php include("common/js.html"); ?>
+	<!-- END: Inclide JS -->
 </body>
 </html>
